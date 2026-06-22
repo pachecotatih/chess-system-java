c2bc0d69a8470f8cad861bee0e42a8d6f5514674
Tatiana Pacheco Barreto <tatianapacheco09@gmail.com>
2026-06-22 10:34:58 -0300

    Moving pieces

--- src/application/App.java
+++ src/application/App.java
@@ -1,11 +1,26 @@
 package application;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) throws Exception {
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+            System.out.println();
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+            System.out.println();
+            ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+        }
+
     }
 }
